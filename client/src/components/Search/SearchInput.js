ff08b659f74e4b68ae0d631baa8f58e046a474e9
Isaev Mustafa <mustafaisaev3@gmail.com>
2023-06-07 14:41:36 +0300

    Making searching from header search input

--- client/src/components/Search/SearchInput.js
+++ client/src/components/Search/SearchInput.js
@@ -1,21 +1,30 @@
 import React, {useState} from 'react'
 import { BsSearch } from 'react-icons/bs'
-import { useDispatch } from 'react-redux'
-import { searchFile } from '../../store/ducks/files/actions'
+import { useHistory } from 'react-router-dom'
 
 const SearchInput = (props) => {
-  const [searchName, setSerachName] = useState('') 
-  const dispatch = useDispatch()
+  const [searchValue, setSerachValue] = useState('') 
+  const history = useHistory()
+
   const searchInputHandler = (e) => {
-    setSerachName(e.target.value)
-    
+    setSerachValue(e.target.value)
   }
+
+  const handleSeacrh = () => {
+    if (!searchValue.length) {
+      return
+    }
+
+    history.push(`/search?search=${searchValue}`)
+
+  }
+
   return (
     <div className='w-[500px] h-[50px] px-3 bg-white rounded-2xl flex items-center overflow-hidden'>
-        <div onClick={() => {dispatch(searchFile(searchName))}}>
-            <BsSearch size={20} color={'#8997a1'} />
+        <input type="text" value={searchValue} onChange={searchInputHandler} className='h-full w-full pl-3 outline-none' {...props}/>        
+        <div className={`p-2 rounded-md ${searchValue.length ? 'text-white bg-[#36a1ea]' : 'text-[#8997a1]'}`} onClick={handleSeacrh}>
+            <BsSearch size={20} />
         </div>
-        <input type="text" value={searchName} onChange={searchInputHandler} className='h-full w-full pl-3 outline-none' {...props}/>        
     </div>
   )
 }
